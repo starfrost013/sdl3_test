0fefe8185f4b962b5733546a3c2a68ea38023d3b
starfrost013 <mario64crashed@gmail.com>
2026-02-15 01:41:44 +0000

    teardown client on disconnect

--- core/net/server/server_auth.cpp
+++ core/net/server/server_auth.cpp
@@ -32,7 +32,7 @@ namespace Capy
 
         Logging_LogChannel("Client is attempting to connect from IP %s...", LogChannel::Debug, ip);
 
-        NetMsg netMsg = NetFactory_CreateServerHelloPacket();
+        NetMsg serverHello = NetFactory_CreateServerHelloPacket();
         NetHelloStatus helloStatus = NetHelloStatus::HELLO_OK;
 
         if (numClients >= MAX_CLIENTS)
@@ -41,20 +41,24 @@ namespace Capy
             helloStatus = NetHelloStatus::HELLO_TOO_MANY;
         }
 
-        netMsg.Write<uint8_t>(static_cast<uint8_t>(helloStatus));
+        serverHello.Write<uint8_t>(static_cast<uint8_t>(helloStatus));
 
         if (helloStatus == NetHelloStatus::HELLO_OK)
         {
+            Logging_LogChannel("Connection accepted!", LogChannel::Debug, ip);
+
             Client* client = new Client();
             clients[numClients] = client;
 
-            client->serverOnly.address = netMsg.addr;
+            client->serverOnly.address = serverHello.addr;
             
             numClients++;
+
+            SendMessage(serverHello, msg->addr);
         }
         else
         {
-            SendMessage(NetFactory_CreateDisconnectPacket(NetCastType::NET_CAST_TO_CLIENT), netMsg.addr);
+            SendMessage(NetFactory_CreateDisconnectPacket(NetCastType::NET_CAST_TO_CLIENT), msg->addr);
         }
     }
 
@@ -62,5 +66,10 @@ namespace Capy
     {
         NET_UnrefAddress(client->serverOnly.address); 
         delete client;
+
+        clients[numClients] = nullptr;
+
+        if (numClients != 0)
+            numClients--;
     }
 }
